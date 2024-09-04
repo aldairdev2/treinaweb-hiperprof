a01203eb560460d2607e12f661418935b039f4a7
aldairdev2 <aldairvicente077@gmail.com>
2024-09-04 13:12:56 -0300

    Criando pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
 import PageTitle  from "@components/data_display/PageTitle";
+import {TextField, Icon, Button} from "@mui/material";
+import { BoxButtons, HomeContainer } from "@styles/pages/index.styles";
 
 export default function Home() {
   return (
-    <div>
-      <PageTitle title="TITULO" subtitle="subtitulo exemplo"/>
-    </div>
+    <HomeContainer>
+      <PageTitle 
+        title="ENCONTRE O PROFESSOR IDEAL PARA VOCÊ!"
+        subtitle="Pesquise pelo o professor ideal para você"
+        />
+
+        <TextField sx={{mt : 3, mb: 1 }}
+        label={"Encontre um Professor"}
+        InputProps={{
+          startAdornment:
+            <Icon>search  </Icon>
+          
+        }}
+        fullWidth
+        required
+        />
+        <BoxButtons>
+        <Button type="submit" variant="contained">Buscar o professor</Button>
+        </BoxButtons>
+    </HomeContainer>
   )
 }
